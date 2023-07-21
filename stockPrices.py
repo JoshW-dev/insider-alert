f0b81212f50811cd5a679321c091c96a95bd2569
Josh thinkpad <joshuafwade@gmail.com>
2023-07-21 11:32:36 -0600

    add functionality to combine stock data into single csv file

--- stockPrices.py
+++ stockPrices.py
@@ -5,13 +5,25 @@ import pandas as pd
 def get_stock_data(ticker, start_date, end_date):
     directory = 'stockData'
     stock_data = yf.download(ticker, start=start_date, end=end_date)
+    # Add a column with the ticker symbol
+    stock_data['Ticker'] = ticker
     # Save the data to a CSV file
     stock_data.to_csv(f'{directory}/{ticker}_data.csv')
     return stock_data
 
+def combine_csv_files(directory):
+    # Create an empty DataFrame to store the combined data
+    combined_data = pd.DataFrame()
+    # Get a list of all CSV files in the directory
+    csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
+    # Read each CSV file and append it to the combined data
+    for file in csv_files:
+        data = pd.read_csv(f'{directory}/{file}')
+        combined_data = combined_data.append(data)
+    # Save the combined data to a CSV file
+    combined_data.to_csv(f'{directory}/combined_data.csv', index=False)
 
-#most recent 10 cluster buys as of July 21
-
+# The most recent 10 cluster buys as of July 21
 ticker_date_ranges = [
     ('MLKN', '2023-06-18', '2023-07-21'),
     ('CAPIX', '2023-05-23', '2023-07-21'),
@@ -27,3 +39,6 @@ ticker_date_ranges = [
 
 for ticker, start_date, end_date in ticker_date_ranges:
     get_stock_data(ticker, start_date, end_date)
+
+# Combine all the CSV files
+combine_csv_files('stockData')
