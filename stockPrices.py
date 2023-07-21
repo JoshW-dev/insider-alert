4bdcbcac8b17fe3554fe2d87c10d9ec3fce4b67d
Josh thinkpad <joshuafwade@gmail.com>
2023-07-21 11:25:19 -0600

    Add 10 most recent cluster buys for back analysis

--- stockPrices.py
+++ stockPrices.py
@@ -10,3 +10,20 @@ def get_stock_data(ticker, start_date, end_date):
     return stock_data
 
 
+#most recent 10 cluster buys as of July 21
+
+ticker_date_ranges = [
+    ('MLKN', '2023-06-18', '2023-07-21'),
+    ('CAPIX', '2023-05-23', '2023-07-21'),
+    ('CVGW', '2023-06-14', '2023-07-21'),
+    ('CDZI', '2023-06-06', '2023-07-21'),
+    ('EPSN', '2023-06-06', '2023-07-21'),
+    ('CNXC', '2023-06-05', '2023-07-21'),
+    ('KGS', '2023-06-03', '2023-07-21'),
+    ('TURN', '2023-04-18', '2023-07-21'),
+    ('OKE', '2023-05-29', '2023-07-21'),
+    ('CMT', '2023-05-28', '2023-07-21')
+]
+
+for ticker, start_date, end_date in ticker_date_ranges:
+    get_stock_data(ticker, start_date, end_date)
